536f51f05fca33fa9817048b7971466da2570216
Lorenzo Bodmer <lorenzobodmer@me.com>
2025-03-24 15:29:04 +0100

    Added Movetable for white

--- include/Engine.h
+++ include/Engine.h
@@ -1,11 +1,16 @@
 #ifndef ENGINE_H
 #define ENGINE_H
 
+#include <array>
+
 class Board;  // Forward declaration
 
 class Engine {
 public:
     void initialize(Board* board);
+    
+    // Add the declaration for normalMoveTableWhite
+    static const std::array<std::array<int, 2>, 26> normalMoveTableWhite;
 };
 
 #endif // ENGINE_H
